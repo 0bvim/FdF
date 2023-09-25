cce590b4ee8a6b6e8c02b2ec1a51b0fd550e5638
vde-frei <vde-frei@student.42sp.org.br>
2023-09-25 04:21:03 -0300

    feat: installed mlx in machine, started a windows and printed a red square.  Changes to be committed: 	new file:   MLX42 	new file:   fdf.h 	new file:   fputs.c 	new file:   main.c
     Changes not staged for commit:
    	modified:   MLX42 (untracked content)

---
+++ fdf.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   fdf.h                                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vde-frei <vde-frei@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/25 03:38:26 by vde-frei          #+#    #+#             */
+/*   Updated: 2023/09/25 03:43:12 by vde-frei         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FDF_H
+# define FDF_H
+
+#include "MLX42/include/MLX42/MLX42.h"
+#include <unistd.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <stdio.h>
+
+#endif
---
+++ main.c
@@ -0,0 +1,53 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vde-frei <vde-frei@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/25 03:37:50 by vde-frei          #+#    #+#             */
+/*   Updated: 2023/09/25 04:20:57 by vde-frei         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "fdf.h"
+
+# define WIDTH 800
+# define HEIGHT 600
+
+/* exit the program as failure.*/
+static void	ft_error(void)
+{
+	fprintf(stderr, "%s", mlx_strerror(mlx_errno));
+	exit (EXIT_FAILURE);
+}
+
+/* print the window width and height. */
+static void	ft_hook(void *param)
+{
+	const mlx_t	*mlx = param;
+	printf("WIDTH: %d | HEIGHT: %d\n", mlx->width, mlx->height);
+}
+
+int32_t	main(void)
+{
+	mlx_set_setting(MLX_MAXIMIZED, false);
+	mlx_t	*mlx = mlx_init(WIDTH, HEIGHT, "FdF", true);
+	if (!mlx)
+		ft_error();
+	write(1, "a", 1);
+	mlx_image_t	*img = mlx_new_image(mlx, 256, 256);
+	if (!img || (mlx_image_to_window(mlx, img, 0, 0) < 0))
+		ft_error();
+	for(int i = 0; i < 99; i++)
+	{
+		for(int j = 0; j < 99; j++)
+			mlx_put_pixel(img, i, j, 0xFF0000FF);
+	}
+	
+	//mlx_loop_hook(mlx, ft_hook, mlx);
+	mlx_loop(mlx);
+	mlx_terminate(mlx);
+	return (0);
+}
+

203b87e07c33b913b9d224ee9e0b6f05c9cdebf3
Vinicius Pereira <niviciusdev@gmail.com>
2023-09-26 10:18:39 -0300

    fix: improved function and learnd which each function does  Changes to be committed: 	modified:   main2.c

--- main2.c
+++ main2.c
@@ -4,9 +4,11 @@
 
 static void	ft_hook(void *param);
 void	closew(mlx_key_data_t key, void *param);
+void	ft_line(mlx_image_t *image, mlx_t *param, uint32_t color);
 
 int	main(void)
 {
+	uint32_t	color = 0x57328900;
 	// Open window maximized
 	mlx_set_setting(MLX_MAXIMIZED, false);
 
@@ -18,28 +20,23 @@ int	main(void)
 	// Get the window image
 	mlx_image_t	*image = mlx_new_image(mlx, WIDTH, HEIGHT);
 	
-	// Set the line color
-	mlx_put_pixel (image, 10, 10, 0xFF0000FF);
+	// Set the line color, commented here, using in function ft_line.
+	//mlx_put_pixel (image, 10, 10, color);
 	
 	// Display the image
-	mlx_image_to_window(mlx, image, 250, 250);
+	mlx_image_to_window(mlx, image, 0, 0);
 
-	// Wait for a key press
+	// Destroy the window and wait for a key press
 	mlx_key_hook(mlx, closew, mlx);
 
-	//mlx_loop_hook(mlx, ft_hook, mlx);
-	// Destroy the window
+	// loop to print line in the window
+	ft_line(image, mlx, color);	
+	//mlx_loop_hook(mlx, ft_hook, image);
 	mlx_loop(mlx);
 	mlx_terminate(mlx);
 }
 
-//static void	ft_hook(void *param)
-//{
-//	mlx_t	*mlx = param;
-//
-//	if (mlx_is_key_down(mlx, MLX_KEY_SPACE))
-//		mlx_close_window(mlx);
-//}
+//static void	ft_hook(void *param) // finish this function
 
 void	closew(mlx_key_data_t key, void *param)
 {
@@ -47,4 +44,13 @@ void	closew(mlx_key_data_t key, void *param)
 
 	if (key.key == MLX_KEY_ESCAPE)
 		mlx_terminate(mlx);
+}
+
+void	ft_line(mlx_image_t *image, mlx_t *param, uint32_t color)
+{
+	for(int i = 0; i < 750; i++)
+	{
+		for(int j = 0; j < 550; j++)
+			mlx_put_pixel(image, i, j, color++);
+	}
 }

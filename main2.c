257b4cad0609f4ca170ebde67d67de3a7af180b1
vde-frei <vde-frei@student.42sp.org.br>
2023-09-28 04:33:46 -0300

    feat: implemented a function that print a square  Changes to be committed: 	modified:   main2.c

--- main2.c
+++ main2.c
@@ -8,7 +8,7 @@ void	ft_line(mlx_image_t *image, mlx_t *param, uint32_t color);
 
 int	main(void)
 {
-	uint32_t	color = 0x57328900;
+	uint32_t	color = 0xFF0000FF;
 	// Open window maximized
 	mlx_set_setting(MLX_MAXIMIZED, false);
 
@@ -24,7 +24,7 @@ int	main(void)
 	//mlx_put_pixel (image, 10, 10, color);
 	
 	// Display the image
-	mlx_image_to_window(mlx, image, 0, 0);
+	mlx_image_to_window(mlx, image, 10, 10);
 
 	// Destroy the window and wait for a key press
 	mlx_key_hook(mlx, closew, mlx);
@@ -48,11 +48,43 @@ void	closew(mlx_key_data_t key, void *param)
 	}
 }
 
+typedef struct s_xyz t_xyz;
+struct s_xyz
+{
+	uint32_t	x;
+	uint32_t	y;
+	uint32_t	z;
+	t_xyz		*next;
+};
+
 void	ft_line(mlx_image_t *image, mlx_t *param, uint32_t color)
 {
-	for(int i = 0; i < 750; i++)
+	t_xyz	map;
+
+	map.x = 0;
+	map.y = 0;
+	map.z = 0;
+	map.next = NULL;
+	for(map.x = 0; map.x < 50; map.x++)
 	{
-		for(int j = 0; j < 550; j++)
-			mlx_put_pixel(image, i, j, color++);
+		mlx_put_pixel(image, map.x, map.y, color);
+	}
+
+	map.y = 50;
+	for( map.x = 0; map.x < 50; map.x++)
+	{
+		mlx_put_pixel(image, map.x, map.y, color);
+	}
+
+	map.x = 0;
+	for( map.y = 0; map.y < 50; map.y++)
+	{
+		mlx_put_pixel(image, map.x, map.y, color);
+	}
+
+	map.x = 50;
+	for( map.y = 0; map.y < 50; map.y++)
+	{
+		mlx_put_pixel(image, map.x, map.y, color);
 	}
 }
